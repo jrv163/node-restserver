fe76780570cc677fd9bf99f7ba52fa3d9eb741cc
Jamer Rodríguez <103492822+jrv163@users.noreply.github.com>
2023-05-01 17:45:50 -0500

    Default port updated

--- models/server.js
+++ models/server.js
@@ -6,7 +6,7 @@ class Server {
 
     constructor(){
         this.app = express();
-        this.PORT = process.env.PORT;
+        this.PORT = process.env.PORT || 3000;
         this.usuariosPath = '/api/usuarios';
 
         // Middlewares
